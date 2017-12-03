c7d048e1a289a6404c6909dc8258fda2e5238c50
colyk <colyk@yandex.ru>
2017-12-03 02:39:24 +0100

    cought some bugs

--- location.py
+++ location.py
@@ -4,45 +4,45 @@ import requests
 import json
 import os
 
-def get_average_location():
-    url = 'http://api.mylnikov.org/geolocation/wifi?bssid='
+url = 'http://api.mylnikov.org/geolocation/wifi?bssid='
 
+def get_average_location():
     raw_text = subprocess.check_output("netsh wlan show all")
     decoded_text = raw_text.decode('utf-8', 'replace')
-    BSSIDs = re.findall(r'\S{2}:\S{2}:\S{2}:\S{2}:\S{2}:\S{2}', decoded_text)[2:] # 1 is your mac, 2 is BSSID of connected wifi
-    k = 0
+    BSSIDs = re.findall(r'\S{2}:\S{2}:\S{2}:\S{2}:\S{2}:\S{2}', decoded_text)[2:] # 1 is your mac, 2> are BSSIDs of APs
+    points = 0
     lat = 0
     lon = 0
     for BSSID in BSSIDs:
         r = requests.get(url+BSSID)
         response = r.text
         json_response = json.loads(response)
-        if(json_response['result'] == 404):
+        if(json_response['result'] != 200):
             continue
-        k += 1
+        points += 1
         # print(json_response)
         lat += json_response['data']['lat']
         lon += json_response['data']['lon']
-    return str(lat/k)+','+str(lon/k)
+    if points == 0:
+        raise ValueError('There are no AP nearby!')
+    return str(lat / points) + ',' + str(lon / points)
 
 
 
 def get_location():
-    url = 'http://api.mylnikov.org/geolocation/wifi?bssid='
-
     raw_text = subprocess.check_output("netsh wlan show interfaces")
     decoded_text = raw_text.decode('utf-8', 'replace')
     macs = re.findall(r'\S{2}:\S{2}:\S{2}:\S{2}:\S{2}:\S{2}', decoded_text)
-    BSSID = macs[1]
+    BSSID = macs[1] # 2 is BSSID of conected AP
 
     r = requests.get(url+BSSID)
     response = r.text
     json_response = json.loads(response)
-    if(json_response['result'] == 404):
-        return None
+    if(json_response['result'] != 200):
+        raise ValueError('There are no AP nearby!')
     lat = json_response['data']['lat']
     lon = json_response['data']['lon']
-    return str(lat)+','+str(lon)
+    return str(lat) + ',' + str(lon)
 
 
 def main():
@@ -55,3 +55,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
